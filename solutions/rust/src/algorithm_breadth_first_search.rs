f225397f8687eda720efd713290d2f3f97853246
Jeremy Stanley <git@azulus.com>
2019-04-14 23:41:42 -0700

    stubbed breadth first search

---
+++ solutions/rust/src/algorithm_breadth_first_search.rs
@@ -0,0 +1,15 @@
+struct Node {
+    edges: Vec<Node>,
+    value: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        assert!(true);
+    }
+
+}
--- solutions/rust/src/main.rs
+++ solutions/rust/src/main.rs
@@ -1,4 +1,5 @@
 mod algorithm_binary_search;
+mod algorithm_breadth_first_search;
 
 fn main() {
     println!("Hello, world!");
