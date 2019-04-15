4b28e823abd80247d1817fcf96fd76dd052df081
Jeremy Stanley <git@azulus.com>
2019-04-14 23:19:14 -0700

    stubbing out test

--- solutions/rust/src/algorithm_binary_search.rs
+++ solutions/rust/src/algorithm_binary_search.rs
@@ -1,13 +1,5 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+fn binary_search() -> Result<usize, String>{
+    Err("start with failure".to_string())
 }
 
 #[cfg(test)]
@@ -15,18 +7,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle { width: 8, height: 7 };
-        let smaller = Rectangle { width: 5, height: 1 };
+    fn simple() {
+        let result = binary_search();
 
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle { width: 8, height: 7 };
-        let smaller = Rectangle { width: 5, height: 1 };
-
-        assert!(!smaller.can_hold(&larger));
+        match result {
+            Ok(_) => assert!(true),
+            Err(e) => panic!("Encountered error \"{}\"", e)
+        }
     }
 }
