3438a34cc94124f35aca89153d0737a7e4695127
Jeremy Stanley <git@azulus.com>
2019-04-14 22:58:38 -0700

    adding gitignore

---
+++ solutions/rust/target/debug/deps/rust-8be7a83c835366ea.d
@@ -0,0 +1,5 @@
+/src/solutions/rust/target/debug/deps/rust-8be7a83c835366ea: src/main.rs
+
+/src/solutions/rust/target/debug/deps/rust-8be7a83c835366ea.d: src/main.rs
+
+src/main.rs:
---
+++ solutions/rust/target/debug/rust-8be7a83c835366ea.d
@@ -0,0 +1,1 @@
+/src/solutions/rust/target/debug/rust-8be7a83c835366ea: /src/solutions/rust/src/main.rs
