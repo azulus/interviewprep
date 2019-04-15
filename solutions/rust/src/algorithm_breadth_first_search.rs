0ced4bba1a5971e45f66ff6b6d3154dcea20cbb0
Jeremy Stanley <git@azulus.com>
2019-04-14 23:57:28 -0700

    stubbing out impl for binary search

--- solutions/rust/src/algorithm_breadth_first_search.rs
+++ solutions/rust/src/algorithm_breadth_first_search.rs
@@ -1,6 +1,19 @@
 struct Node {
-    edges: Vec<Node>,
+    left: Box<Option<Node>>,
+    right: Box<Option<Node>>,
     value: i32,
+}
+
+impl Node {
+    fn add_child(&mut self, node: &mut Node) -> &mut Node {
+
+        self
+    }
+
+    fn find_val(&self, search_val: i32) -> Result<Node, String> {
+
+        Err("This is a test".to_string())
+    }
 }
 
 #[cfg(test)]
